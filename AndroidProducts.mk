3d7e16da5bfd421150c3a65e01d135542139f178
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-11 19:19:05 +0000

    a01core: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: samsung Model: SM-M013F Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a01core.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a01core-user \
+    omni_a01core-userdebug \
+    omni_a01core-eng
