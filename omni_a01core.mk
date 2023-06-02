67a7fb62206e9617b109fac67a5e0885a127f90a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-06-03 01:59:29 +0200

    a01core: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-A013F Script version: 3.0.0

---
+++ omni_a01core.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a01core device
+$(call inherit-product, device/samsung/a01core/device.mk)
+
+PRODUCT_DEVICE := a01core
+PRODUCT_NAME := omni_a01core
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A013F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a01corecis-user 10 QP1A.190711.020 A013FXXU7AWA1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a01corecis/a01core:10/QP1A.190711.020/A013FXXU7AWA1:user/release-keys
